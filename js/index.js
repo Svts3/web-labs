161247b9aaa8860d504c0a51aeeda133ccb5bf5f
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2022-09-28 23:44:22 +0300

    Added data validation

--- js/index.js
+++ js/index.js
@@ -32,97 +32,97 @@ let devices = [];
 
 let totalPrice = 0;
 const addDevice = ({ name, price, weight, type }) => {
-    
-    let generatedId = uuid.v1();
 
-    const device = {
-        id: generatedId,
-        name,
-        price,
-        weight,
-        type
-    }
+  let generatedId = uuid.v1();
 
-    devices.push(device);
-    addItemToPage(device);
+  const device = {
+    id: generatedId,
+    name,
+    price,
+    weight,
+    type
+  }
 
-    createButton.style.display="block";
-    createForm.style.display="none";
-    formTitle.style.display="block";
-    totalPriceH2.style.display="block";
+  devices.push(device);
+  addItemToPage(device);
+
+  createButton.style.display = "block";
+  createForm.style.display = "none";
+  formTitle.style.display = "block";
+  totalPriceH2.style.display = "block";
 }
 
 const countTotalPrice = (devices) => {
-    totalPrice = devices.reduce((accumulator, device) => accumulator + parseFloat(device.price), 0);
+  totalPrice = devices.reduce((accumulator, device) => accumulator + parseFloat(device.price), 0);
 };
 
- 
 
 
 
 
 
-createButton.addEventListener("click", (event)=>{
 
-    event.preventDefault();
+createButton.addEventListener("click", (event) => {
 
-    createButton.style.display="none";
-    createForm.style.display="block";
-    formTitle.style.display="none";
+  event.preventDefault();
+
+  createButton.style.display = "none";
+  createForm.style.display = "block";
+  formTitle.style.display = "none";
 
 });
 
 
 
 submitButton.addEventListener("click", (event) => {
-    event.preventDefault();
-  
-    const { name, price, weight, type } = getInputValues();
-  
-    clearInputs();
-  
-    addDevice({
-        name,
-        price,
-        weight,
-        type
-    });
-    countTotalPrice(devices);
-    totalPriceH2.innerHTML=`Total Price: ${totalPrice}`;
-    console.log(devices);
+  event.preventDefault();
+
+  const { name, price, weight, type } = getInputValues();
+
+  clearInputs();
+
+  addDevice({
+    name,
+    price,
+    weight,
+    type
   });
+  countTotalPrice(devices);
+  totalPriceH2.innerHTML = `Total Price: ${totalPrice}`;
+  console.log(devices);
+});
 
-findButton.addEventListener("click", (event)=>{
-    deleteTableRows();
-    event.preventDefault();
-    let foundDevices = devices.filter(d=>d.name === findInput.value);
-    
-    renderItemsList(foundDevices);
+findButton.addEventListener("click", (event) => {
+  deleteTableRows();
+  event.preventDefault();
+  let foundDevices = devices.filter(d => d.name === findInput.value);
 
-}) 
+  renderItemsList(foundDevices);
 
-
-sortItemsByASC.addEventListener("click", (event)=>{
-    deleteTableRows();
-   const sortedDevices = devices.sort((d1, d2)=>{
-        return d2.price - d1.price;
-   });
-    renderItemsList(sortedDevices);
-
-}) 
-
-sortItemsByDESC.addEventListener("click", (event)=>{
-    deleteTableRows();
-    const sortedDevices = devices.sort((d1, d2)=>{
-         return d1.price - d2.price;
-    });
-     renderItemsList(sortedDevices);
- 
- }) 
+})
 
 
+sortItemsByASC.addEventListener("click", (event) => {
+  deleteTableRows();
+  const sortedDevices = devices.sort((d1, d2) => {
+    return d2.price - d1.price;
+  });
+  renderItemsList(sortedDevices);
 
- const itemTemplate = ({ id, name, price, weight, type }) =>`
+})
+
+sortItemsByDESC.addEventListener("click", (event) => {
+  deleteTableRows();
+  const sortedDevices = devices.sort((d1, d2) => {
+    return d1.price - d2.price;
+  });
+  renderItemsList(sortedDevices);
+
+})
+
+
+
+const itemTemplate = ({ id, name, price, weight, type }) => `
       <div id=${id} class="item-card">
       <img class="item-image" src="./images/office_equipment_image.svg">
       <p id="name">${name}</p>
@@ -133,69 +133,69 @@ sortItemsByDESC.addEventListener("click", (event)=>{
       <button onclick="deleteDevice('${id}')">Delete</button>
       </div>`
 
-function deleteDevice(id){
+function deleteDevice(id) {
   for (let i = 0; i < devices.length; i++) {
     deleteTableRows();
     if (devices[i].id === id) {
-          devices.splice(i, 1);
-          break;
-      }
-      
+      devices.splice(i, 1);
+      break;
     }
-    countTotalPrice(devices);
-    totalPriceH2.innerHTML=`Total Price: ${totalPrice}`;
-    renderItemsList(devices);
+
+  }
+  countTotalPrice(devices);
+  totalPriceH2.innerHTML = `Total Price: ${totalPrice}`;
+  renderItemsList(devices);
   console.log(devices)
 }
 
 
-function updateDevice(id){
+function updateDevice(id) {
 
 
   let deviceToUpdated;
   for (let i = 0; i < devices.length; i++) {
-      if (devices[i].id === id) {
-          deviceToUpdated = devices[i];
-          break;
-      }
+    if (devices[i].id === id) {
+      deviceToUpdated = devices[i];
+      break;
+    }
   }
   console.log(devices)
   console.log(deviceToUpdated);
 
-  createButton.style.display="none";
-  createForm.style.display="block";
-  formTitle.style.display="none";
-  totalPriceH2.style.display="none";
+  createButton.style.display = "none";
+  createForm.style.display = "block";
+  formTitle.style.display = "none";
+  totalPriceH2.style.display = "none";
   nameInput.value = deviceToUpdated.name;
   priceInput.value = deviceToUpdated.price;
   weightInput.value = deviceToUpdated.weight;
   typeInput.value = deviceToUpdated.type;
 
   deleteDevice(id);
-  
+
   console.log(devices)
   console.log(deviceToUpdated);
-  
+
 }
 
 
- const clearInputs = () => {
+const clearInputs = () => {
   nameInput.value = "";
   priceInput.value = "";
   weightInput.value = "";
   typeInput.value = "";
 };
 
- const addItemToPage = ({ id: id, name, price, weight, type }) => {
+const addItemToPage = ({ id: id, name, price, weight, type }) => {
   items.insertAdjacentHTML(
     "afterbegin",
     itemTemplate({ id, name, price, weight, type })
   );
 
-  
+
 };
 
- const renderItemsList = (items) => {
+const renderItemsList = (items) => {
   items.innerHTML = "";
 
   for (const item of items) {
@@ -203,19 +203,22 @@ function updateDevice(id){
   }
 };
 
- const getInputValues = () => {
-  return {
-    name: nameInput.value,
-    price: priceInput.value,
-    weight: weightInput.value,
-    type: typeInput.value
-  };
+const getInputValues = () => {
+  if (validateInputForms()) {
+
+    return {
+      name: nameInput.value,
+      price: priceInput.value,
+      weight: weightInput.value,
+      type: typeInput.value
+    };
+  }
 };
 
- const deleteTableRows = ()=>{
+const deleteTableRows = () => {
 
   while (itemDiv.firstChild) {
-    itemDiv.innerHTML="";
+    itemDiv.innerHTML = "";
   }
 }
 
@@ -224,30 +227,30 @@ function updateDevice(id){
 
 const validateInputForms = () => {
   if (!nameInput.value.trim().length) {
-      alert("Name cannot be empty")
-      return false;
-  }
-  if (!weightInput.value.trim().length) {
-      alert("Weight cannot be empty")
-      return false;
+    alert("Name cannot be empty")
+    return false;
   }
   if (!priceInput.value.trim().length) {
-      alert("Price cannot be empty")
-      return false;
-  }
-  if (!typeInput.value.trim().length) {
-      alert("Type cannot be empty")
-      return false;
+    alert("Price cannot be empty")
+    return false;
   }
   if (priceInput.value < 0) {
-      alert("Price can't be negative")
-      return false;
+    alert("Price can't be negative")
+    return false;
+  }
+  if (!weightInput.value.trim().length) {
+    alert("Weight cannot be empty")
+    return false;
   }
   if (weightInput.value < 0) {
     alert("Weight can't be negative")
     return false;
-}
-  
+  }
+  if (!typeInput.value.trim().length) {
+    alert("Type cannot be empty")
+    return false;
+  }
+
   return true;
 };
 
